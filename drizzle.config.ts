988c820016172af490c83ed3b704492359da3061
Mashakkkk <mashamar40@gmail.com>
2026-07-12 17:30:09 +0300

    Add drizzle.config.ts

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+Exit code: 0
+Wall time: 1.6 seconds
+Output:
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  out: "./drizzle",
+  schema: "./db/schema.ts",
+  dialect: "sqlite",
+});
+
